ff2b4b154a4264df72384733ffb9f54ee8506014
mimifuwa <mimifuwa.cc@gmail.com>
2025-04-22 13:22:31 +0900

    chore: 細かい文言を修正3

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -23,7 +23,7 @@ export default function Page() {
         alt="Saiten"
         className="w-32 h-32 rounded-2xl shadow-md"
       />
-      <h1 className="text-white font-bold text-2xl mt-4">Saiten - 採点しよう。</h1>
+      <h1 className="text-white font-bold text-4xl mt-8 mb-6">採点しよう。</h1>
       <p className="text-gray-300 text-sm text-left mt-4 w-full">
         PDFの解答例から、選択問題の答え合わせができます。LLM（Gemini-1.5-flash）で解答例を生成しているため精度が良くない場合があります。
       </p>
